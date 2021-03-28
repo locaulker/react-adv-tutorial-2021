b0692eb8fff3671b23751140bc81aaaa05e787e9
Lawrence O. Caulker <locaulker@gmail.com>
2021-03-28 03:01:17 -0400

    end of L57 - Conditional Returns

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,28 @@ import React, { useState } from "react"
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("")
+  const [isError, setIsError] = useState(false)
 
-  return <h2>short circuit Evaluation</h2>
+  // const firstValue = text || "hello world"
+  // const secondValue = text && "hello world"
+
+  return (
+    <>
+      <h1>{text || "John Doe"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Toddle Error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>There's an error...</p>
+      ) : (
+        <div>
+          <h2>There's No Error</h2>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default ShortCircuit
