4d689d8316f0ceaf7ac6528c70fb0e7a2bc8da0a
Lawrence O. Caulker <locaulker@gmail.com>
2021-03-28 09:56:32 -0400

    adding form input objects to an array

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -9,9 +9,25 @@ const ControlledInputs = () => {
   const [firstName, setFirstName] = useState("")
   const [email, setEmail] = useState("")
 
+  const [people, setPeople] = useState([])
+
   const handleSubmit = e => {
     e.preventDefault()
-    console.log(firstName, email)
+
+    if (firstName && email) {
+      // creates a person object
+      const person = { id: new Date().getTime().toString(), firstName, email }
+
+      // adding each person to the people array
+      setPeople(people => {
+        return [...people, person]
+      })
+      // reset the input boxes
+      setFirstName("")
+      setEmail("")
+    } else {
+      console.log("empty values")
+    }
   }
 
   return (
@@ -40,6 +56,16 @@ const ControlledInputs = () => {
           </div>
           <button type="submit">Add Person</button>
         </form>
+
+        {people.map(person => {
+          const { id, firstName, email } = person
+          return (
+            <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          )
+        })}
       </article>
     </>
   )
